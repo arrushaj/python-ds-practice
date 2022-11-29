eaee6950e823326c89e8c4e28d463b884bc2d958
Ardit Rrushaj <arditrrushaj@gmail.com>
2022-11-29 14:30:50 -0600

    Finish up to 15

--- 15_intersection.py
+++ 15_intersection.py
@@ -1,15 +1,24 @@
 def intersection(l1, l2):
     """Return intersection of two lists as a new list::
-    
+
         >>> intersection([1, 2, 3], [2, 3, 4])
         [2, 3]
-        
+
         >>> intersection([1, 2, 3], [1, 2, 3, 4])
         [1, 2, 3]
-        
+
         >>> intersection([1, 2, 3], [3, 4])
         [3]
-        
+
         >>> intersection([1, 2, 3], [4, 5, 6])
         []
     """
+    new_li = []
+
+    for item_l1 in l1:
+        for item_l2 in l2:
+            if item_l1 == item_l2:
+                new_li.append(item_l1)
+                break
+
+    return new_li
